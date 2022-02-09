1f80ef30b4670bfc62d502b9341caeb64c05d96a
Erkan Göçmen <erkangocmen19@gmail.com>
2022-02-09 13:29:19 +0300

    Kıtalar listelendi

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { ApolloClient, InMemoryCache, gql, useQuery } from '@apollo/client';
+import { Container } from 'react-bootstrap';
 
+// initialize a GraphQL client
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  uri: 'https://countries.trevorblades.com'
+});
+
+// write a GraphQL query that asks for names and codes for all countries
+const LIST_CONTINENTS = gql`
+  {
+    continents {
+      name
+      code
+    }
+  }
+`;
+
+// create a component that renders a select input for coutries
 function App() {
+  const [continent, setCountry] = useState('');
+  const { data, loading, error } = useQuery(LIST_CONTINENTS, { client });
+
+  if (loading || error) {
+    return <p>{error ? error.message : 'Loading...'}</p>;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container style={{margin:20}}>
+      <select value={continent} onChange={event => setCountry(event.target.value)}>
+        {data.continents.map(con => (
+          <option key={con.code} value={con.code}>
+            {con.name}
+          </option>
+        ))}
+      </select>
+    </Container>
   );
 }
 
